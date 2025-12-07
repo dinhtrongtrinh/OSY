ecf81c63d4c45109ddf83182372c77311494c1f3
Trong Trinh Dinh <dinhtrongtrinh04@gmail.com>
2025-12-07 15:22:33 +0100

    hw07

--- hw07/osy.pages.fel.cvut.cz-master-3263d798c1ae77a84f8893e36214196c02acccc4-nova/nova/kern/src/ec_syscall.cc
+++ hw07/osy.pages.fel.cvut.cz-master-3263d798c1ae77a84f8893e36214196c02acccc4-nova/nova/kern/src/ec_syscall.cc
@@ -11,21 +11,13 @@ typedef enum {
     sys_thr_yield  = 5,
 } Syscall_numbers;
 
-/**
- * System call handler
- *
- * @param[in] a Value of the `al` register before the system call
- */
 void Ec::syscall_handler (uint8 a)
 {
-    // Get access to registers stored during entering the system - see
-    // entry_sysenter in entry.S
     Sys_regs * r = current->sys_regs();
     Syscall_numbers number = static_cast<Syscall_numbers> (a);
 
     switch (number) {
         case sys_print: {
-            // Tisk řetězce na sériovou linku
             char *data = reinterpret_cast<char*>(r->esi);
             unsigned len = r->edi;
             for (unsigned i = 0; i < len; i++)
@@ -33,7 +25,6 @@ void Ec::syscall_handler (uint8 a)
             break;
         }
         case sys_sum: {
-            // Naprosto nepotřebné systémové volání na sečtení dvou čísel
             int first_number = r->esi;
             int second_number = r->edi;
             r->eax = first_number + second_number;
@@ -43,73 +34,89 @@ void Ec::syscall_handler (uint8 a)
             mword address = r->esi;
             mword old_brk = break_current;
             
-            // Používám malá písmena, aby to nekolidovalo s globálním PAGE_SIZE
             static const mword page_size = 0x1000;
             static const mword page_mask = 0xFFFFF000;
 
-            // 1. Pokud je adresa 0, vracíme aktuální break
             if (address == 0) {
                 r->eax = break_current;
                 break;
             }
 
-            // 2. Kontrola limitů
-            if (address < break_min || address > 0xBFFFF000) {
+            if (address < break_min || address >= 0xBFFFF000) {
                 r->eax = 0;
                 break;
             }
 
-            // 3. Výpočet hranic stránek
             mword old_page_limit = (break_current + page_size - 1) & page_mask;
             mword new_page_limit = (address + page_size - 1) & page_mask;
 
-            // 4. Alokace (zvětšování)
-            if (new_page_limit > old_page_limit) {
-                for (mword vaddr = old_page_limit; vaddr < new_page_limit; vaddr += page_size) {
-                    void *phys = Kalloc::allocator.alloc_page(1, Kalloc::FILL_0);
-                    
-                    if (!phys) {
-                        // Chyba alokace -> Rollback (úklid)
-                        for (mword cleanup = old_page_limit; cleanup < vaddr; cleanup += page_size) {
-                            mword p = Ptab::get_mapping(cleanup);
-                            if (p & Ptab::PRESENT) {
-                                Kalloc::allocator.free_page(reinterpret_cast<void*>(p & page_mask));
-                                Ptab::insert_mapping(cleanup, 0, 0);
+            if (address > old_brk) {
+                if (new_page_limit > old_page_limit) {
+                    for (mword vaddr = old_page_limit; vaddr < new_page_limit; vaddr += page_size) {
+                        void *virt_kernel = Kalloc::allocator.alloc_page(1, Kalloc::FILL_0);
+                        
+                        if (!virt_kernel) {
+                            for (mword cleanup = old_page_limit; cleanup < vaddr; cleanup += page_size) {
+                                mword p = Ptab::get_mapping(cleanup);
+                                if (p & Ptab::PRESENT) {
+                                    mword phys = p & page_mask;
+                                    Kalloc::allocator.free_page(Kalloc::phys2virt(phys));
+                                    Ptab::insert_mapping(cleanup, 0, 0);
+                                }
                             }
+                            r->eax = 0;
+                            goto nbrk_end;
                         }
-                        r->eax = 0;
-                        goto nbrk_end; // Vyskočíme ven
+                        
+                        mword phys = Kalloc::virt2phys(virt_kernel);
+
+                        if (!Ptab::insert_mapping(vaddr, phys, 
+                                             Ptab::PRESENT | Ptab::RW | Ptab::USER)) {
+                            Kalloc::allocator.free_page(virt_kernel);
+                            for (mword cleanup = old_page_limit; cleanup < vaddr; cleanup += page_size) {
+                                mword p = Ptab::get_mapping(cleanup);
+                                if (p & Ptab::PRESENT) {
+                                    mword phys_cleanup = p & page_mask;
+                                    Kalloc::allocator.free_page(Kalloc::phys2virt(phys_cleanup));
+                                    Ptab::insert_mapping(cleanup, 0, 0);
+                                }
+                            }
+                            r->eax = 0;
+                            goto nbrk_end;
+                        }
                     }
-                    
-                    Ptab::insert_mapping(vaddr, reinterpret_cast<mword>(phys), 
-                                         Ptab::PRESENT | Ptab::RW | Ptab::USER);
-                }
-                if (address > break_current) {
-                    memset(reinterpret_cast<void*>(break_current), 0, address - break_current);
                 }
             } 
-            // 5. Dealokace (zmenšování)
-            else if (new_page_limit < old_page_limit) {
-                for (mword vaddr = new_page_limit; vaddr < old_page_limit; vaddr += page_size) {
-                    mword p = Ptab::get_mapping(vaddr);
-                    if (p & Ptab::PRESENT) {
-                        Kalloc::allocator.free_page(reinterpret_cast<void*>(p & page_mask));
-                        Ptab::insert_mapping(vaddr, 0, 0);
+            else if (address < old_brk) {
+                mword zero_limit;
+                if (new_page_limit < old_page_limit) {
+                    zero_limit = new_page_limit;
+                } else {
+                    zero_limit = old_brk;
+                }
+
+                if (zero_limit > address) {
+                    memset(reinterpret_cast<void*>(address), 0, zero_limit - address);
+                }
+
+                if (new_page_limit < old_page_limit) {
+                    for (mword vaddr = new_page_limit; vaddr < old_page_limit; vaddr += page_size) {
+                        mword p = Ptab::get_mapping(vaddr);
+                        if (p & Ptab::PRESENT) {
+                            mword phys = p & page_mask;
+                            Kalloc::allocator.free_page(Kalloc::phys2virt(phys));
+                            Ptab::insert_mapping(vaddr, 0, 0);
+                        }
                     }
                 }
             }
-            // 6. Nastavení nového breaku a návrat starého
-            if (address < break_current) {
-                // Nulujeme od nového konce po starý konec
-                memset(reinterpret_cast<void*>(address), 0, break_current - address);
-            }
+            
             break_current = address;
             r->eax = old_brk;
 
             nbrk_end:
             break;
         }
-        // TODO: Add your system calls here
         default:
             printf ("unknown syscall %d\n", number);
             break;
