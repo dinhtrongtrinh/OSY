677bf17ed76b4316cc7fe3fcfb5348e90d78e986
Trong Trinh Dinh <dinhtrongtrinh04@gmail.com>
2025-12-12 15:56:39 +0100

    malloc - zatim jenom pridani pameti bez hledani

--- hw07/osy.pages.fel.cvut.cz-master-3263d798c1ae77a84f8893e36214196c02acccc4-nova/nova/user/mem_alloc.c
+++ hw07/osy.pages.fel.cvut.cz-master-3263d798c1ae77a84f8893e36214196c02acccc4-nova/nova/user/mem_alloc.c
@@ -51,15 +51,53 @@ static void *nbrk(void *address)
 
 #endif
 
+typedef struct header {
+    unsigned long size;    // Velikost datové části
+    int is_free;           // 1 = volné, 0 = obsazené
+    struct header *next;   // Ukazatel na další blok v seznamu
+    struct header *prev;   // Ukazatel na předchozí blok v seznamu
+} header_t;
+
+static header_t *head = NULL; // Ukazatel na začátek seznamu bloků
+static header_t *tail = NULL; // Ukazatel na konec seznamu bloků
+
 void *my_malloc(unsigned long size)
 {
-    /* TODO: Implement in 11malloc */
-    printf("my_malloc not implemented\n");
-    return (void*)0;
+    if (size == 0) {
+        return NULL; // Nulová velikost, vrátit NULL
+    }
+    unsigned long total_size = size + sizeof(header_t);
+
+    unsigned long current_brk = nbrk(0);
+
+    unsigned long new_nrk = nbrk(current_brk + total_size);
+    if (new_nrk == 0) {
+        return 0; // Selhání alokace paměti
+    }
+
+    header_t *new_block = (header_t *)current_brk;
+    new_block->size = size;
+    new_block->is_free = 0;
+    new_block->next = NULL;
+    if (head == NULL) {
+        head = new_block; // Inicializace seznamu bloků
+        tail = new_block;
+        new_block->prev = NULL;
+    }
+    else{
+        tail->next = new_block; // Přidání nového bloku na konec seznamu
+        new_block->prev = tail;
+        tail = new_block;
+        
+    }
+
+    
+    return (void*)(new_block + 1); // Vrátit ukazatel na datovou část bloku
 }
 
 int my_free(void *address)
 {
+     
     /* TODO: Implement in 11malloc */
     printf("my_free not implemented\n");
     return -1;
